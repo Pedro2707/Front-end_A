59758382452ec899a5785c7ac4f028fe2fb93d58
pedro2707 <pedrocesarmarcia2@gmail.com>
2024-08-15 09:31:09 -0600

    chorse:style

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { BiHome } from "react-icons/bi";
 import { BsPeople } from "react-icons/bs";
 import { Link } from "react-router-dom";
@@ -6,19 +5,19 @@ import { Link } from "react-router-dom";
 const DashboardLayout = ({ children }) => {
   const panelData = [
     {
-      title: 'Inicio',
+      title: "Inicio",
       icon: BiHome,
-      link: '/dashboard',
+      link: "/dashboard",
     },
     {
-      title: 'Personas',
+      title: "Personas",
       icon: BsPeople,
-      link: '/dashboard/personas/1',
+      link: "/dashboard/personas/1",
     },
     {
-      title: 'Citas',
+      title: "Citas",
       icon: BsPeople,
-      link: '/dashboard/citas/1',
+      link: "/dashboard/citas/1",
     },
   ];
 
@@ -29,7 +28,7 @@ const DashboardLayout = ({ children }) => {
         data-drawer-toggle="logo-sidebar"
         aria-controls="logo-sidebar"
         type="button"
-        className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200"
       >
         <span className="sr-only">Open sidebar</span>
         <svg
@@ -49,29 +48,29 @@ const DashboardLayout = ({ children }) => {
 
       <aside
         id="logo-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-white shadow-lg"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-          <a href="https://flowbite.com/" className="flex items-center pl-2.5 mb-5">
+        <div className="h-full px-4 py-6 overflow-y-auto bg-gradient-to-b from-gray-800 to-gray-600">
+          <a href="/" className="flex items-center pl-2.5 mb-8">
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn2rC6mKlm3Wj324Y_Ukz5l7JNcQz-nYvbgA&s"
               className="h-6 mr-3 sm:h-7"
-              alt="Flowbite Logo"
+              alt="Logo"
             />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+            <span className="self-center text-xl font-semibold text-white">
+             Menu
             </span>
           </a>
-          <ul className="space-y-2 font-medium">
+          <ul className="space-y-4">
             {panelData.map(({ title, icon: Icon, link }) => (
               <li key={`left.panel.title.${title}`}>
                 <Link
                   to={link}
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  className="flex items-center p-3 text-white rounded-lg hover:bg-gray-700"
                 >
-                  <Icon className="w-6 h-6" />
-                  <span className="ml-3">{title}</span>
+                  <Icon className="w-6 h-6 text-gray-300" />
+                  <span className="ml-4 text-lg">{title}</span>
                 </Link>
               </li>
             ))}
@@ -79,7 +78,9 @@ const DashboardLayout = ({ children }) => {
         </div>
       </aside>
 
-      <div className="p-4 sm:ml-64 flex-1">
+      <div className="flex-1 p-6 bg-gray-100 sm:ml-64">
+        <div className="flex justify-between items-center mb-8">
+        </div>
         {children}
       </div>
     </div>
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -5,109 +5,22 @@ import { dashboardParams } from "../constans";
 
 const DasboardPage = () => {
   return (
-    <DashboardLayout >
-      
-      <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-        <div class="grid grid-cols-3 gap-4 mb-4">
-          <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-          <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-          <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-        </div>
-        <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div class="grid grid-cols-2 gap-4 mb-4">
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-        </div>
-        <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-              </svg>
-            </p>
-          </div>
-        </div>
+    <DashboardLayout>
+     <div
+        className="flex items-center justify-center bg-cover bg-center"
+        style={{
+          backgroundImage: `url('https://www.hch.tv/wp-content/uploads/2021/08/Hospital-San-Francisco-1.jpg')`,
+          height: "100vh", // Mantiene la altura a 100% de la ventana
+          overflow: "hidden", // Evita que aparezca la barra de desplazamiento
+          margin: 0, // Elimina los márgenes
+          padding: 0, // Elimina el padding
+          position: "relative", // Para un mejor control del contenido
+        }}
+      >
       </div>
-    </DashboardLayout>
+  </DashboardLayout>
+);
+};
 
-  );
-}
+
 export default DasboardPage;
